ff139a7a48d8b640cdaf5e6004cb159dc462ff0f
Michael Wagner <michaelwagner42@googlemail.com>
2018-06-03 20:25:21 +0200

    Added Avalonia project

---
+++ MtgBinders/MtgBinders/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using MtgBinders.ViewModels;
+using MtgBinders.Views;
+
+namespace MtgBinders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
---
+++ MtgBinders/MtgCards/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MtgCards
+{
+    public class Class1
+    {
+    }
+}
