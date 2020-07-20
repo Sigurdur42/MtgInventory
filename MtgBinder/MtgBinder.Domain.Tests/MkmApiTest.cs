9f1bfd080fd972cfe3be26c3a52cbe9072eef799
Michael Wagner <michaelwagner42@outlook.com>
2020-07-20 08:08:48 +0200

    more api tests

--- MtgBinder/MtgBinder.Domain.Tests/MkmApiTest.cs
+++ MtgBinder/MtgBinder.Domain.Tests/MkmApiTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MtgBinder.Domain.Mkm;
 using NUnit.Framework;
 
 namespace MtgBinder.Domain.Tests
@@ -13,7 +14,7 @@ namespace MtgBinder.Domain.Tests
         public void TestMkmApi()
         {
             var target = new RequestHelper();
-            var result = target.MakeRequest();
+            var result = target.MakeRequest(new MkmAuthentication());
 
             Assert.IsNotNull(result);
         }
---
+++ MtgBinder/MtgBinder.Domain/Mkm/MkmApi.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using MtgBinder.Domain.Mkm;
+
+namespace MtgBinder.Domain
+{
+    public class RequestHelper
+    {
+        public string MakeRequest(MkmAuthentication authentication)
+        {
+            var method = "GET";
+            // var url = "https://api.cardmarket.com/ws/v2.0/account";
+            var url = "https://api.cardmarket.com/ws/v2.0/stock/file";
+
+            var request = WebRequest.CreateHttp(url) as HttpWebRequest;
+            var header = new OAuthHeader(authentication);
+            request.Headers.Add(HttpRequestHeader.Authorization, header.getAuthorizationHeader(method, url));
+            request.Method = method;
+
+            var response = request.GetResponse() as HttpWebResponse;
+            var encoding = response.CharacterSet == ""
+                ? Encoding.UTF8
+                : Encoding.GetEncoding(response.CharacterSet);
+
+            var responseString = "";
+            using (var stream = response.GetResponseStream())
+            {
+                var reader = new StreamReader(stream, encoding);
+                responseString = reader.ReadToEnd();
+            }
+
+            // proceed further
+
+            return responseString;
+        }
+    }
+
+    /// <summary>
+    /// Class encapsulates tokens and secret to create OAuth signatures and return Authorization headers for web requests.
+    /// </summary>
+    internal class OAuthHeader
+    {
+        private readonly MkmAuthentication _authentication;
+
+
+        /// <summary>OAuth Signature Method</summary>
+        protected string signatureMethod = "HMAC-SHA1";
+
+        /// <summary>OAuth Version</summary>
+        protected string version = "1.0";
+
+        /// <summary>All Header params compiled into a Dictionary</summary>
+        protected IDictionary<string, string> headerParams;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OAuthHeader(MkmAuthentication authentication)
+        {
+            _authentication = authentication;
+            // String nonce = Guid.NewGuid().ToString("n");
+            var nonce = "53eb1f44909d6";
+
+            // String timestamp = (DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds.ToString();
+            var timestamp = "1407917892";
+
+            // Initialize all class members
+            this.headerParams = new Dictionary<string, string>();
+            this.headerParams.Add("oauth_consumer_key", _authentication.AppToken);
+            this.headerParams.Add("oauth_token", _authentication.AccessToken);
+            this.headerParams.Add("oauth_nonce", nonce);
+            this.headerParams.Add("oauth_timestamp", timestamp);
+            this.headerParams.Add("oauth_signature_method", this.signatureMethod);
+            this.headerParams.Add("oauth_version", this.version);
+        }
+
+        /// <summary>
+        /// Pass request method and URI parameters to get the Authorization header value
+        /// </summary>
+        /// <param name="method">Request Method</param>
+        /// <param name="url">Request URI</param>
+        /// <returns>Authorization header value</returns>
+        public string getAuthorizationHeader(string method, string url)
+        {
+            // Add the realm parameter to the header params
+            this.headerParams.Add("realm", url);
+
+            // Start composing the base string from the method and request URI
+            var baseString = method.ToUpper()
+                             + "&"
+                             + Uri.EscapeDataString(url)
+                             + "&";
+
+            // Gather, encode, and sort the base string parameters
+            var encodedParams = new SortedDictionary<string, string>();
+            foreach (var parameter in this.headerParams)
+            {
+                if (false == parameter.Key.Equals("realm"))
+                {
+                    encodedParams.Add(Uri.EscapeDataString(parameter.Key), Uri.EscapeDataString(parameter.Value));
+                }
+            }
+
+            // Expand the base string by the encoded parameter=value pairs
+            var paramStrings = new List<string>();
+            foreach (var parameter in encodedParams)
+            {
+                paramStrings.Add(parameter.Key + "=" + parameter.Value);
+            }
+            var paramString = Uri.EscapeDataString(string.Join<string>("&", paramStrings));
+            baseString += paramString;
+
+            // Create the OAuth signature
+            var signatureKey = Uri.EscapeDataString(_authentication.AppSecret) + "&" + Uri.EscapeDataString(_authentication.AccessSecret);
+            var hasher = HMACSHA1.Create("HMACSHA1");
+            hasher.Key = Encoding.UTF8.GetBytes(signatureKey);
+            var rawSignature = hasher.ComputeHash(Encoding.UTF8.GetBytes(baseString));
+            var oAuthSignature = Convert.ToBase64String(rawSignature);
+
+            // Include the OAuth signature parameter in the header parameters array
+            this.headerParams.Add("oauth_signature", oAuthSignature);
+
+            // Construct the header string
+            var headerParamStrings = new List<string>();
+            foreach (var parameter in this.headerParams)
+            {
+                headerParamStrings.Add(parameter.Key + "=\"" + parameter.Value + "\"");
+            }
+            var authHeader = "OAuth " + string.Join<string>(", ", headerParamStrings);
+
+            return authHeader;
+        }
+    }
+}
