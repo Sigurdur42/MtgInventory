6212a89d5198eb0910292a2639c24cb944953998
Michael Wagner <michaelwagner42@googlemail.com>
2020-08-09 11:30:47 +0200

    Added Scryfall API Wrapper

---
+++ MtgInventory/ScryfallApiServices/IScryfallService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ScryfallApi.Client.Models;
+
+namespace MtgBinder.Domain.Scryfall
+{
+    public interface IScryfallService
+    {
+        IEnumerable<Set> RetrieveSets();
+
+        Card[] RetrieveCardsForSetCode(string setCode);
+
+        Card[] RetrieveCardsByCardName(string cardName, SearchOptions.RollupMode rollupMode);
+    }
+}
