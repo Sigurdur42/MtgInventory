94d4894a8af703a80f13558dc83a07f27f22d3d6
Michael Wagner <Michael.Wagner@wisetechglobal.com>
2020-10-29 15:33:09 +0100

    Added catch block

--- MtgInventory/MtgInventory/MtgInventory/Program.cs
+++ MtgInventory/MtgInventory/MtgInventory/Program.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Logging.Serilog;
 using Avalonia.ReactiveUI;
 
 namespace MtgInventory
 {
-    class Program
+    internal class Program
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
-
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
                 .LogToDebug()
                 .UseReactiveUI();
+
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args)
+        {
+            try
+            {
+                BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+            }
+            catch (Exception error)
+            {
+                // TODO: Actual log
+                Console.WriteLine($"Unhandled exception caught: {error}");
+            }
+        }
     }
 }
