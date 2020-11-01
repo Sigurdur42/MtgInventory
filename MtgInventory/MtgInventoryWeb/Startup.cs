b8f6d484f11f5e7b2f50a31b6af6be66a31c6da4
Michael Wagner <Michael.Wagner@wisetechglobal.com>
2020-11-01 11:04:42 +0100

    Added references Added logging config

--- MtgInventory/MtgInventoryWeb/Startup.cs
+++ MtgInventory/MtgInventoryWeb/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using MtgInventoryWeb.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MtgInventoryWeb
 {
@@ -21,15 +16,6 @@ namespace MtgInventoryWeb
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
-        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -54,6 +40,23 @@ namespace MtgInventoryWeb
             {
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+
+            services.AddLogging(cfg =>
+            {
+                cfg.AddConfiguration(Configuration.GetSection("Logging"));
+                cfg.AddConsole();
+                cfg.AddDebug();
+                //// cfg.AddProvider(new PanelLogSinkProvider());
             });
         }
     }
