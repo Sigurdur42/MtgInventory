432466351b57278d5e7c30ffaea14ebec8b1d3a0
michael wagner <michaelwagner42@googlemail.com>
2020-12-14 07:48:58 +0100

    Code cleanup

--- MtgInventory/Scryfall/ScryfallApiServices/ScryfallConfiguration.cs
+++ MtgInventory/Scryfall/ScryfallApiServices/ScryfallConfiguration.cs
@@ -7,12 +7,15 @@ namespace ScryfallApiServices
     {
         public int UpdateSetCacheInDays { get; set; } = 14;
         public int UpdateCardCacheInDays { get; set; } = 28;
+        
+        public int UpdateCardCachePriceInDays { get; set; } = 2;
 
         public string DumpSettings()
         {
             var builder = new StringBuilder();
             builder.AppendLine($"{nameof(UpdateSetCacheInDays)}: {UpdateSetCacheInDays}");
             builder.AppendLine($"{nameof(UpdateCardCacheInDays)}: {UpdateCardCacheInDays}");
+            builder.AppendLine($"{nameof(UpdateCardCachePriceInDays)}: {UpdateCardCachePriceInDays}");
             return builder.ToString();
         }
 
@@ -24,6 +27,11 @@ namespace ScryfallApiServices
         public bool IsCardOutdated(DateTime? setUpdateDate)
         {
             return DateTime.Now.AddDays(-1 * UpdateCardCacheInDays).Date > (setUpdateDate ?? DateTime.MinValue).Date;
+        }
+        
+        public bool IsCardPriceOutdated(DateTime? setUpdateDate)
+        {
+            return DateTime.Now.AddDays(-1 * UpdateCardCachePriceInDays).Date > (setUpdateDate ?? DateTime.MinValue).Date;
         }
     }
 }
