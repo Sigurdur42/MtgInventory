82734cb41a4ab67b8872a140dc87475584534688
michael wagner <michaelwagner42@googlemail.com>
2020-12-25 12:44:21 +0100

    Fixed land version in MKM resolver

--- MtgDatabase/MkmMapper.cs
+++ MtgDatabase/MkmMapper.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Web;
 using MtgDatabase.Models;
 
@@ -10,8 +12,13 @@ namespace MtgDatabase
 
     public class MkmMapper : IMkmMapper
     {
+        private readonly Database.MtgDatabase _database;
         public static string UrlPrefix => "https://www.cardmarket.com/de/Magic/Products/Singles/";
 
+        public MkmMapper(Database.MtgDatabase database)
+        {
+            _database = database;
+        }
         public string GenerateUrl(QueryableMagicCard card)
         {
             // - name
@@ -19,9 +26,29 @@ namespace MtgDatabase
             var setName = PatchSetName(card.SetName, card.Name);
             var cardName = PatchCardName(card.SetName, card.Name);
 
+            if (card.IsBasicLand)
+            {
+                cardName = ResolveBasicLandCardName(card);
+            }
+            
             // TODO: some cards and sets have different names
 
             return HttpUtility.HtmlEncode($"{UrlPrefix}{setName}/{cardName}");
+        }
+
+        private string ResolveBasicLandCardName(QueryableMagicCard card)
+        {
+            var allOfThisTypeAndSet = _database.Cards?.Query()
+                .Where(c => c.Name == card.Name && c.SetCode == card.SetCode && c.Language.Equals("EN", StringComparison.InvariantCultureIgnoreCase))
+                .OrderBy(c => c.CollectorNumber)
+                .ToArray()
+                ?? Array.Empty<QueryableMagicCard>();
+
+            var version = 1 + allOfThisTypeAndSet
+                .TakeWhile(cardOfSet => cardOfSet.CollectorNumber != card.CollectorNumber)
+                .Count();
+
+            return $"{card.Name}-V-{version}";
         }
 
         public static string PatchSetName(string setName, string cardName) =>
