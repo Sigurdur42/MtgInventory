0c873ac772420205a542bc1b7be8a055bb1d1741
Michael Wagner <MichaelWagner42@googlemail.com>
2021-01-22 07:33:26 +0100

    Added initial DI

--- MtgInventoryWpf/App.xaml.cs
+++ MtgInventoryWpf/App.xaml.cs
@@ -5,13 +5,54 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace MtgInventoryWpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private readonly IHost host;
+
+        public App()
+        {
+            // The DI configuration here has been build based on
+            // https://marcominerva.wordpress.com/2019/11/07/update-on-using-hostbuilder-dependency-injection-and-service-provider-with-net-core-3-0-wpf-applications/
+            host = Host.CreateDefaultBuilder()
+                   .ConfigureServices((context, services) =>
+                   {
+                       ConfigureServices(context.Configuration, services);
+                   })
+
+                   .Build();
+        }
+
+        private void ConfigureServices(IConfiguration configuration,
+            IServiceCollection services)
+        {
+            // ...
+            services.AddSingleton<MainWindow>();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await host.StartAsync();
+
+            var mainWindow = host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (host)
+            {
+                await host.StopAsync(TimeSpan.FromSeconds(5));
+            }
+
+            base.OnExit(e);
+        }
     }
 }
