2d15a7b84b4a94fd2143f5c541fe9dfca5589cb0
Michael Wagner <MichaelWagner42@googlemail.com>
2021-01-23 08:29:06 +0100

    Added display of database info

--- MtgInventoryWpf/CardSearchViewModel.cs
+++ MtgInventoryWpf/CardSearchViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PropertyChanged;
 
 namespace MtgInventoryWpf
 {
+    [AddINotifyPropertyChangedInterface]
     public class CardSearchViewModel
     {
     }
--- MtgInventoryWpf/DatabaseInfoViewModel.cs
+++ MtgInventoryWpf/DatabaseInfoViewModel.cs
@@ -1,11 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using MtgDatabase;
+using MtgDatabase.Models;
+using PropertyChanged;
 
 namespace MtgInventoryWpf
 {
+    [AddINotifyPropertyChangedInterface]
     public class DatabaseInfoViewModel
     {
-        public string HeaderName => ":database:"; // database icon in font-awesome
+        private readonly IMtgDatabaseService _mtgDatabaseService;
+
+
+        public DatabaseSummary? DatabaseSummary { get; set; }
+        public string LastUpdate { get; set; }
+        private string _noUpdateYet = "Never";
+
+        public DatabaseInfoViewModel(
+            IMtgDatabaseService mtgDatabaseService)
+        {
+            LastUpdate = _noUpdateYet;
+            Task.Factory.StartNew(UpdateDatabaseStatistics);
+            _mtgDatabaseService = mtgDatabaseService;
+        }
+
+        private void UpdateDatabaseStatistics()
+        {
+            DatabaseSummary = _mtgDatabaseService.GetDatabaseSummary();
+            var updated = DatabaseSummary?.LastUpdated ?? DateTime.MinValue;
+            LastUpdate = updated == DateTime.MinValue ? _noUpdateYet : updated.ToShortDateString();
+        }
     }
 }
