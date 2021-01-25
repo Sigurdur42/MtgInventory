0f42711773b056bb95a7ce48216d54dca776add3
Michael Wagner <michael.wagner@wisetechglobal.com>
2021-01-25 10:16:58 +0100

    Removed Blazor project Code cleanup

--- ScryfallApiServices/Models/ScryfallBulkCard.cs
+++ ScryfallApiServices/Models/ScryfallBulkCard.cs
@@ -14,31 +14,31 @@ namespace ScryfallApiServices.Models
 
         [JsonPropertyName("lang")] public string Language { get; set; } = "";
 
-        [JsonPropertyName("set")] public string Set { get; set; }
+        [JsonPropertyName("set")] public string Set { get; set; } = "";
 
-        [JsonPropertyName("set_name")] public string SetName { get; set; }
+        [JsonPropertyName("set_name")] public string SetName { get; set; } = "";
 
-        [JsonPropertyName("collector_number")] public string CollectorNumber { get; set; }
+        [JsonPropertyName("collector_number")] public string CollectorNumber { get; set; } = "";
 
-        [JsonPropertyName("type_line")] public string TypeLine { get; set; }
+        [JsonPropertyName("type_line")] public string TypeLine { get; set; } = "";
 
         [JsonPropertyName("printed_type_line")]
-        public string PrintedTypeLine { get; set; }
+        public string PrintedTypeLine { get; set; } = "";
 
         [JsonPropertyName("oracle_text")] public string OracleText { get; set; } = "";
 
         public DateTime? released_at { get; set; }
 
-        [JsonPropertyName("mana_cost")] public string ManaCost { get; set; }
+        [JsonPropertyName("mana_cost")] public string ManaCost { get; set; } = "";
 
-        [JsonPropertyName("legalities")] public Dictionary<string, string> Legalities { get; set; }
+        [JsonPropertyName("legalities")] public Dictionary<string, string> Legalities { get; set; } = new Dictionary<string, string>();
 
         [JsonPropertyName("reserved")] public bool Reserved { get; set; }
 
-        [JsonPropertyName("image_uris")] public Dictionary<string, Uri> ImageUris { get; set; }
+        [JsonPropertyName("image_uris")] public Dictionary<string, Uri> ImageUris { get; set; } = new Dictionary<string, Uri>();
 
         [JsonPropertyName("digital")] public bool Digital { get; set; }
 
-        [JsonPropertyName("rarity")] public string Rarity { get; set; }
+        [JsonPropertyName("rarity")] public string Rarity { get; set; } = "";
     }
 }
